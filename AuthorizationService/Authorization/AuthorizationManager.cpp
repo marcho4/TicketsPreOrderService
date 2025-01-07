1c5270e06e591556da9fb474359260002a498830
Nazar <nvzakrevskii@edu.hse.ru>
2025-01-07 13:48:44 +0300

    authorization_manager almost finished

--- AuthorizationService/Authorization/AuthorizationManager.cpp
+++ AuthorizationService/Authorization/AuthorizationManager.cpp
@@ -1,21 +1,36 @@
 #include "AuthorizationManager.h"
 
-void AuthorizationImpl::AuthorizationRequest(const httplib::Request& req, httplib::Response& res,
+void AuthorizationManager::AuthorizationRequest(const httplib::Request& req, httplib::Response& res,
                               Database& db) {
+    int id = std::stoi(req.matches[1]);
+
     auto parsed = json::parse(req.body);
     std::string login = parsed["login"];
     std::string password = parsed["password"];
-    std::string request = "SELECT FROM AuthorizationService.AuthorizationData "
-                          "WHERE login = '" + login + "' AND password = '" + password + "';";
-    pqxx::result result = db.executeQuery(request);
-    if (result.empty()) {
+    pqxx::result password_hash;
+
+    try {
+        std::string query = "SELECT password FROM AuthorizationService.AuthorizationData WHERE id = $1";
+        password_hash = db.executeQueryWithParams(query, id);
+    } catch (const std::exception& e) {
+        res.status = 500;
+        res.set_content(R"({"status": "Server error"})", "application/json");
+        return;
+    }
+
+    if (password_hash.empty()) { // проверка на существование пользователя
         res.status = 403; // 403 - forbidden
         res.set_content(R"({"status": "Access denied"})", "application/json");
         return;
     }
-    Authorization(login, password);
+
+    if (!validatePassword(password, password_hash[0][0].c_str())) { // проверка на корректность пароля
+        res.status = 403;
+        res.set_content(R"({"status": "Access denied"})", "application/json");
+        return;
+    }
 }
 
-void AuthorizationImpl::Authorization(const std::string& login, const std::string& password) {
-    // генерируем и отправляем jwt-токен
+bool AuthorizationManager::validatePassword(const std::string& password, const std::string& hashed_password) {
+    return bcrypt::validatePassword(password, hashed_password);
 }
--- AuthorizationService/Authorization/AuthorizationManager.h
+++ AuthorizationService/Authorization/AuthorizationManager.h
@@ -2,13 +2,13 @@
 #include "../../libraries/nlohmann/json.hpp"
 #include <pqxx/pqxx>
 #include "../postgres/PostgresProcessing.h"
-
-class AuthorizationImpl {
+#include "../../libraries/Bcrypt.cpp/include/bcrypt.h"
+class AuthorizationManager {
     using json = nlohmann::json;
 
 public:
     static void AuthorizationRequest(const httplib::Request& req, httplib::Response& res,
                               Database& db);
 
-    static void Authorization(const std::string& login, const std::string& password);
+    static bool validatePassword(const std::string& password, const std::string& hashed_password);
 };
