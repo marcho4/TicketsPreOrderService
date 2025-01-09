a28e2c01f33d2568402bdd050ad945cec944df76
Nazar <nvzakrevskii@edu.hse.ru>
2025-01-09 14:35:32 +0300

    user database tests accepted

--- AuthorizationService/tests/config.py
+++ AuthorizationService/tests/config.py
@@ -1,2 +1,6 @@
 BASE_URL = "http://localhost:8081"
-DATABASE_PATH = ""
+DATABASE_CONFIG = {
+    "dbname": "db_org_registr",
+    "host": "localhost",
+    "port": 5432
+}
--- AuthorizationService/tests/test_user_registration.py
+++ AuthorizationService/tests/test_user_registration.py
@@ -1,9 +1,10 @@
 import pytest
 import requests
 import sqlite3
-
+import psycopg2
 from requests.exceptions import RequestException
-from config import BASE_URL, DATABASE_PATH
+from psycopg2.extras import RealDictCursor
+from config import BASE_URL, DATABASE_CONFIG
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -15,12 +16,6 @@ def check_server_availability():
     except RequestException:
         pytest.fail("Невозможно подключиться к серверу. Возможно он не работает")
 
-# @pytest.fixture(scope="module")
-# def db_connection():
-#     """Подключение к базе данных перед выполнением тестов и закрытие после"""
-#     conn = sqlite3.connect(DATABASE_PATH)
-#     yield conn
-#     conn.close()
 
 @pytest.mark.parametrize('data, expected_status_code, expected_status_message', [
     # тест на пустые поля
@@ -96,37 +91,37 @@ def test_register_duplicate_user(data, expected_status_code, expected_status_mes
         assert response_data["email"] == "markdergilev@gmail.com"
 
 
-# @pytest.mark.parametrize('data, expected_status_code, expected_status_message, should_have_been_inserted', [
-#     (
-#             {"name": "Nazar",
-#              "last_name": "",
-#              "email": "nazarzakrevski@gmail.com"},
-#             400,  # status code
-#             "fill every field!",  # response
-#             False
-#     )
-#     (
-#         {"name": "Nazar",
-#          "last_name": "",
-#          "email": "nazarzakrevski@gmail.com"},
-#         200,  # status code
-#         "User registered",  # response
-#         True
-#     )
-# ])
-# def test_proper_database_work(data, expected_status_code, expected_status_message, should_have_been_inserted):
-#     response = requests.post(f"{BASE_URL}/register_user", json=data)
-#     assert response.status_code == expected_status_code
-#     response_data = response.json()
-#     assert response_data["status"] == expected_status_message
-#     cursor = db_connection.cursor()
-#     cursor.execute("SELECT * FROM AuthorizationService.AuthorizationData WHERE email = ?", (data["email"],))
-#     ans = cursor.fetchone()
-#     if response.status_code == 200:
-#         assert response_data["name"] == "Nazar"
-#         assert response_data["last_name"] == "Zakrevski"
-#         assert response_data["email"] == "nazarzakrevski@gmail.com"
-#     if should_have_been_inserted:
-#         assert ans is not None, "Пользователь должен существовать в базе данных"
-#         assert ans[3] == "nazarzakrevski@gmail.com"
-#
+@pytest.fixture(scope="module")
+def db_connection():
+    """Подключение к базе данных перед выполнением тестов."""
+    conn = psycopg2.connect(
+        dbname=DATABASE_CONFIG["dbname"],
+        host=DATABASE_CONFIG["host"],
+        port=DATABASE_CONFIG["port"],
+    )
+    yield conn
+    conn.close()
+
+
+@pytest.mark.parametrize('data, expected_status_code, expected_status_message', [
+    ({"name": "Nazar", "last_name": "", "email": "nazarzakrevski123@gmail.com"}, 400, "fill every field!"),
+    ({"name": "Nazar", "last_name": "Zakrevski", "email": "nazarzakrevski1235@gmail.com"}, 200, "User registered"),
+    ({"name": "Nazar", "last_name": "Zakrevski", "email": "invalid_email"}, 400, "email already exists or email invalid"),
+])
+def test_proper_database_work(data, expected_status_code, expected_status_message, db_connection):
+    response = requests.post(f"{BASE_URL}/register_user", json=data)
+    assert response.status_code == expected_status_code
+    response_data = response.json()
+    assert response_data["status"] == expected_status_message
+
+    cursor = db_connection.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM AuthorizationService.AuthorizationData WHERE email = %s", (data["email"],))
+    db_result = cursor.fetchone()
+
+    if expected_status_code == 200:
+        assert db_result is not None, "User should be present in the database"
+        assert db_result["email"] == data["email"]
+        assert db_result["login"] is not None
+        assert db_result["password"] is not None
+    else:
+        assert db_result is None, "User should not be present in the database for invalid inputs"
