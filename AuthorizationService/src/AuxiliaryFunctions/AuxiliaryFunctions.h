ec08da7db75c77490f34c5f1e177187b27490cb3
Zakrevski Nazar <144466355+naziikk@users.noreply.github.com>
2025-01-20 20:24:03 +0300

    Revert "Nazar zakrevski"

--- AuthorizationService/src/AuxiliaryFunctions/AuxiliaryFunctions.h
+++ AuthorizationService/src/AuxiliaryFunctions/AuxiliaryFunctions.h
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <regex>
-#include <fstream>
 
 class AuxiliaryFunctions {
 public:
---
+++ OrganizerService/MatchCreating/CreateMatch/Creator.h
@@ -0,0 +1,16 @@
+#include "../../../libraries/httplib.h"
+#include "../../../libraries/nlohmann/json.hpp"
+#include "pqxx/pqxx"
+#include "../../postgres/PostgresProcessing.h"
+
+class Creator {
+    using json = nlohmann::json;
+
+public:
+    void CreateMatchRequest(const httplib::Request& req, httplib::Response& res,
+                                       Database& db);
+
+    bool CheckMatchExistence(int organizer_id, const std::string& team_home, const std::string& team_away, const std::string& match_date, Database& db);
+};
+
+
