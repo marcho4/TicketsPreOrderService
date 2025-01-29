a1e2cb702cc477fd035e9d2218a5ba442af3134a
Zakrevski Nazar <144466355+naziikk@users.noreply.github.com>
2025-01-29 13:01:17 +0300

    bag fixed

--- AdminService/main.cpp
+++ AdminService/main.cpp
@@ -13,15 +13,15 @@ int main() {
         pqxx::connection connection_(connect);
         pqxx::work worker(connection_);
 
-        server.Get("admin/pending_requests", [&](const httplib::Request& req, httplib::Response& res) {
+        server.Get("/admin/pending_requests", [&](const httplib::Request& req, httplib::Response& res) {
             GetRequests::GetOrganizersRequestList(req, res, db);
         });
 
-        server.Post("admin/process_organizer", [&](const httplib::Request& req, httplib::Response& res) {
+        server.Post("/admin/process_organizer", [&](const httplib::Request& req, httplib::Response& res) {
             ProcessRequest::ProcessOrganizerRequest(req, res, db);
         });
 
-        server.Post("admin/add_organizer_request", [&](const httplib::Request& req, httplib::Response& res) {
+        server.Post("/admin/add_organizer_request", [&](const httplib::Request& req, httplib::Response& res) {
             AddRequest::AddOrganizerRequest(req, res, db);
         });
 
