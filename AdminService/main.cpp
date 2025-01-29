93b481722a1f3bb8211ba1ac2ac11c91e34cc2f4
Zakrevski Nazar <144466355+naziikk@users.noreply.github.com>
2025-01-29 11:10:36 +0300

    admin processing improved

--- AdminService/main.cpp
+++ AdminService/main.cpp
@@ -6,7 +6,7 @@
 int main() {
     try {
         httplib::Server server;
-        // Обработчик preflight OPTIONS запросов
+
         server.Options(".*", [&](const httplib::Request& req, httplib::Response& res) {
             res.set_header("Access-Control-Allow-Origin", "http://localhost:3000");
             res.set_header("Access-Control-Allow-Credentials", "true");
@@ -16,7 +16,6 @@ int main() {
             res.status = 200;
         });
 
-        // Функция для установки CORS-заголовков
         auto set_cors_headers = [&](httplib::Response& res) {
             res.set_header("Access-Control-Allow-Origin", "http://localhost:3000");
             res.set_header("Access-Control-Allow-Credentials", "true");
@@ -24,30 +23,36 @@ int main() {
             res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
             res.set_header("Content-Type", "application/json");
         };
-        std::string connect = "dbname=orchestrator host=admin_postgres user=postgres password=postgres port=5432";
+        std::string connect = "dbname=orchestrator host=postgres user=postgres password=postgres port=5432";
         Database db(connect);
+        db.initDbFromFile("src/postgres/pending_organizers.sql");
         db.initDbFromFile("src/postgres/pending_organizers.sql");
         pqxx::connection connection_(connect);
         pqxx::work worker(connection_);
 
         server.Get("admin/pending_requests", [&](const httplib::Request& req, httplib::Response& res) {
+            set_cors_headers(res);
             GetRequests::GetOrganizersRequestList(req, res, db);
         });
 
         server.Post("admin/process_organizer", [&](const httplib::Request& req, httplib::Response& res) {
+            set_cors_headers(res);
             ProcessRequest::ProcessOrganizerRequest(req, res, db);
         });
 
         server.Post("admin/add_organizer_request", [&](const httplib::Request& req, httplib::Response& res) {
+            set_cors_headers(res);
             AddRequest::AddOrganizerRequest(req, res, db);
         });
 
-        server.Get("/is_working", [&db, &set_cors_headers](const httplib::Request& req, httplib::Response& res) {
+        server.Get("/is_working", [&](const httplib::Request& req, httplib::Response& res) {
             set_cors_headers(res);
             res.status = 200;
             res.set_content("Server is working", "text/plain");
         });
 
+        std::cout << "Server is listening http://localhost:8003" << '\n';
+        server.listen("0.0.0.0", 8003);
         std::cout << "Server is listening http://localhost:8003" << '\n';
         server.listen("0.0.0.0", 8003);
     } catch (const std::exception& e) {
