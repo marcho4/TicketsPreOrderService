b95e7520b32fb072490906937c916972e171ee22
Zakrevski Nazar <144466355+naziikk@users.noreply.github.com>
2025-02-04 10:17:13 +0300

    try tests

--- OrganizerService/tests/config.py
+++ OrganizerService/tests/config.py
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 BASE_URL = "http://organizer:8002"
 =======
 BASE_URL = "http://localhost:8002"
 >>>>>>> 9a32d90 (documentation added)
+=======
+BASE_URL = "http://organizer:8002"
+>>>>>>> 51711ba (try tests)
 
 DATABASE_CONFIG = {
     "dbname": "orchestrator",
