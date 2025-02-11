7eaec4b4e704a5c50dff55f5f56b94181aa84b27
Zakrevski Nazar <144466355+naziikk@users.noreply.github.com>
2025-02-11 23:53:24 +0300

    добавлен триггер на перенос предзаказов в посещенные

--- UserService/src/postgres/user_personal_account.sql
+++ UserService/src/postgres/user_personal_account.sql
@@ -19,8 +19,7 @@ CREATE TABLE IF NOT EXISTS Users.AttendedEvents (
     user_id UUID REFERENCES Users.UsersData(user_id),
     match_id UUID NOT NULL,
     ticket_id UUID NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS Users.Preorders (
@@ -32,3 +31,25 @@ CREATE TABLE IF NOT EXISTS Users.Preorders (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+------------------------------------------------triggers-----------------------------------------------------------
+
+-- Триггер для автоматического переноса предзаказов в посещенные события
+CREATE OR REPLACE FUNCTION add_preorder_to_attended_events()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.match_date <= NOW() THEN
+        INSERT INTO Users.AttendedEvents (user_id, match_id, ticket_id)
+        VALUES (NEW.user_id, NEW.match_id, NEW.ticket_id);
+        DELETE FROM Users.Preorders WHERE id = NEW.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Триггер будет срабатывать при вставке или обновлении записей в Preorders
+CREATE TRIGGER auto_move_to_attended
+    AFTER INSERT OR UPDATE ON Users.Preorders
+    FOR EACH ROW
+    EXECUTE FUNCTION add_preorder_to_attended_events();
