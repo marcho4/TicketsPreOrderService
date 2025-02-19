a6a409abd47f93468bb605e384bcb1995f7f7e48
marcho4 <markdergilev@yandex.ru>
2025-02-19 16:10:39 +0300

    fixed config

--- orchestrator/src/api/tickets/config.rs
+++ orchestrator/src/api/tickets/config.rs
@@ -1,7 +1,9 @@
 use actix_web::web;
 use crate::api::tickets::add_tickets::add_tickets;
+use crate::api::tickets::cancel_preorder::cancel_preorder;
 use crate::api::tickets::get_available_tickets::get_available_tickets;
 use crate::api::tickets::get_tickets_by_user::get_tickets_by_user;
+use crate::api::tickets::preorder::preorder_ticket;
 
 pub fn config_services(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -9,5 +11,7 @@ pub fn config_services(cfg: &mut web::ServiceConfig) {
             .service(get_available_tickets)
             .service(get_tickets_by_user)
             .service(add_tickets)
+            .service(preorder_ticket)
+            .service(cancel_preorder)
     );
 }
